d7b6a0e92cbee6ae53e1a2f9790ee4cfc3d562bb
markjwalton <markjwalton@users.noreply.github.com>
2025-09-09 01:16:43 +0100

    Update files - Version 23

---
+++ src/components/BookingWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookingWidget = () => {
+  return (
+    <div>
+      {/* Your booking widget code will go here */}
+    </div>
+  );
+};
+
+export default BookingWidget;
